6622764b4c287a553c3172ef62afed871faa1e35
Eric Hale <eric_hale@comcast.net>
2020-06-03 10:47:57 -0600

    Add CookBook class with tests

---
+++ test/cook_book_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+require './lib/cook_book'
+
+class RecipeTest < Minitest::Test
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @cookbook = CookBook.new
+  end
+
+  def test_it_exists
+    assert_instance_of CookBook, @cookbook
+  end
+
+  def test_it_has_attributes
+    assert_equal [], @cookbook.recipes
+  end
+end
